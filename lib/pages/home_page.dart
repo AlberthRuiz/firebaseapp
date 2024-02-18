f8722d26a52037d68ba8405522a725c232395337
Christiam Mendoza <ch.alb.rz@hotmail.com>
2024-02-18 12:30:34 -0500

    firebase

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -5,6 +6,53 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    CollectionReference productsReferences =
+        FirebaseFirestore.instance.collection("products");
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("HOLA"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                productsReferences.get().then(
+                  (value) {
+                    QuerySnapshot productsCollection = value;
+                    List<QueryDocumentSnapshot> docs = productsCollection.docs;
+                    docs.forEach((element) {
+                      print(element.data());
+                    });
+                  },
+                );
+              },
+              child: Text("Traer data"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                productsReferences.add({
+                  "marca": "puma",
+                  "modelo": "Puma100air",
+                  "precio": 230,
+                });
+              },
+              child: Text("Insertar 1"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                productsReferences.doc("bividis").set({
+                  "marca": "boston",
+                  "modelo": "lino algodon",
+                  "precio": 23,
+                });
+              },
+              child: Text("Insertar 2"),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
